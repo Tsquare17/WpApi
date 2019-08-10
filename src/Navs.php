c94128b61825e878953c3a8d3175dfd9eadee91e
Trevor Thompson <tsquare1717@gmail.com>
2019-08-10 00:01:40 -0400

    Make Nav iterate over items, and return only id, title, and href

--- src/Navs.php
+++ src/Navs.php
@@ -6,11 +6,24 @@ class Navs {
 
 	/**
 	 * @param $url string
+	 * @param $route string
+	 * @param $menu string
 	 *
 	 * @return array
 	 */
-	public static function all( $url ): array
+	public static function all( $url, $route, $menu ): array
 	{
-		return json_decode(Curl::get($url . '/wp-json/navroute/menu'), true);
+		$nav = json_decode(Curl::get("{$url}/wp-json/{$route}/{$menu}"), true);
+
+		$navItems = [];
+		foreach ($nav as $key => $value) {
+			$navItems[] = [
+				'ID' => $value['ID'],
+				'Title' => $value['title'],
+				'href' => $value['url'],
+			];
+		}
+
+		return $navItems;
 	}
 }
