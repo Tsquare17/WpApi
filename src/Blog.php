dc58dc94457829a709989f0f042ab263262471dd
Trevor Thompson <tsquare1717@gmail.com>
2019-08-11 21:37:14 -0400

    Did Work

--- src/Blog.php
+++ src/Blog.php
@@ -3,10 +3,21 @@
 namespace Tsquare\WpApi;
 
 /**
- * Call to necessary endpoints and form an easy to use object for displaying posts on the blog page.
+ * Call to necessary endpoints and form an easy to use array of a specified number of objects for displaying posts on the blog page.
  *
  * @package Tsquare\WpApi
  */
 class Blog {
 
+	protected $url;
+
+	public function __construct($url = null)
+	{
+		$this->url = $url ?: env('APP_WP_API');
+	}
+
+	public function getPosts( $perPage = 100, $page = 1 ): Blog
+	{
+		// need to refactor Get class to use optional parameters
+	}
 }
