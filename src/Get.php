cc8b172096723fe888583ee4435b23548cba79ce
Trevor Thompson <tsquare1717@gmail.com>
2019-08-19 23:21:03 -0400

    Move class Curl into Get.

--- src/Get.php
+++ src/Get.php
@@ -12,7 +12,7 @@ class Get {
 	 */
 	public static function all( $url, $endpoint )
 	{
-		return json_decode(Curl::get("{$url}/wp-json/wp/v2/{$endpoint}"), false);
+		return json_decode(self::curl("{$url}/wp-json/wp/v2/{$endpoint}"), false);
 	}
 
 	/**
@@ -24,11 +24,31 @@ class Get {
 	 */
 	public static function byId( $url, $endpoint, $id )
 	{
-		return json_decode(Curl::get("{$url}/wp-json/wp/v2/{$endpoint}/{$id}"), false);
+		return json_decode(self::curl("{$url}/wp-json/wp/v2/{$endpoint}/{$id}"), false);
 	}
 
 	public static function bySlug( $url, $endpoint, $slug ): array
 	{
-		return json_decode(Curl::get("{$url}/wp-json/wp/v2/{$endpoint}?slug={$slug}"), false);
+		return json_decode(self::curl("{$url}/wp-json/wp/v2/{$endpoint}?slug={$slug}"), false);
+	}
+
+	/**
+	 * @param $url string
+	 *
+	 * @return string
+	 */
+	private static function curl( $url ): string
+	{
+		try {
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_URL, $url);
+			$result = curl_exec($ch);
+			curl_close($ch);
+		} catch(\Exception $e) {
+			return $e->getMessage();
+		}
+
+		return $result;
 	}
 }
